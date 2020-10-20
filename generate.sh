78eaf1f4aaf3ac6a1acabb6e26852d4980e5f4b9
sapphi-red <green@sapphi.red>
2020-10-20 21:17:57 +0900

    Update: change package name

--- generate.sh
+++ generate.sh
@@ -3,11 +3,14 @@
 set -u
 
 # clean
-FILES=$(find . -maxdepth 1 -type f ! -name "*.md" ! -name ".*" ! -name "*.sh" ! -name "config.yaml" ! -name "go.mod" ! -name "go.sum")
-echo $FILES | xargs rm
+FILES=$(find . -maxdepth 1 -type f ! -name "*.md" ! -name ".*" ! -name "*.sh" ! -name "config.yaml" ! -name "openapitools.json" ! -name "go.mod" ! -name "go.sum")
+echo $FILES | xargs --no-run-if-empty rm
 
 # build
-npx @openapitools/openapi-generator-cli generate -i https://raw.githubusercontent.com/traPtitech/traQ/master/docs/v3-api.yaml -g go -c config.yaml
+npx @openapitools/openapi-generator-cli generate \
+  -i https://raw.githubusercontent.com/traPtitech/traQ/master/docs/v3-api.yaml \
+  -g go \
+  -c config.yaml
 go fmt ./...
 
 rm -rf ./docs
