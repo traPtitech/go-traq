f3be8c6f08e576d4749653322963937829a01ac2
sapphi-red <green@sapphi.red>
2022-03-22 02:29:03 +0900

    Chore: do not use npx

--- generate.sh
+++ generate.sh
@@ -1,13 +1,25 @@
 #!/bin/sh
 
-set -u
+set -eux
+
+OPENAPI_GENERATOR_VERSION='4.3.1'
 
 # clean
-FILES=$(find . -maxdepth 1 -type f ! -name "*.md" ! -name ".*" ! -name "*.sh" ! -name "config.yaml" ! -name "openapitools.json" ! -name "go.mod" ! -name "go.sum")
+FILES=$(find . -maxdepth 1 -type f ! -name "*.md" ! -name ".*" ! -name "*.sh" ! -name "config.yaml" ! -name "go.mod" ! -name "go.sum" ! -name "*.jar")
 echo $FILES | xargs --no-run-if-empty rm
 
+# fetch openapi-generator
+NEEDS_FETCH='1'
+if [ -e 'openapi-generator-cli.jar' ]; then
+  VERSION_DOWNLOADED=`java -jar openapi-generator-cli.jar version`
+  NEEDS_FETCH=`test $OPENAPI_GENERATOR_VERSION != $VERSION_DOWNLOADED && echo '1' || echo '0'`
+fi
+if [ $NEEDS_FETCH = '1' ]; then
+  wget https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/$OPENAPI_GENERATOR_VERSION/openapi-generator-cli-$OPENAPI_GENERATOR_VERSION.jar -O openapi-generator-cli.jar
+fi
+
 # build
-npx @openapitools/openapi-generator-cli generate \
+java -jar openapi-generator-cli.jar generate \
   -i https://raw.githubusercontent.com/traPtitech/traQ/master/docs/v3-api.yaml \
   -g go \
   -c config.yaml
