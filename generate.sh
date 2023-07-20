0f380e4ac077e652a1fa4d8005e8e4b2de74dead
ras0q <ras0q@users.noreply.github.com>
2023-07-20 09:34:09 +0900

    :wrench: build by docker

--- generate.sh
+++ generate.sh
@@ -8,21 +8,12 @@ OPENAPI_GENERATOR_VERSION=$(cat ./.openapi-generator/VERSION)
 mv README.md README.md.bak
 cat ./.openapi-generator/FILES | xargs --no-run-if-empty rm -f
 
-# fetch openapi-generator
-NEEDS_FETCH='1'
-if [ -e 'openapi-generator-cli.jar' ]; then
-  VERSION_DOWNLOADED=`java -jar openapi-generator-cli.jar version`
-  NEEDS_FETCH=`test $OPENAPI_GENERATOR_VERSION != $VERSION_DOWNLOADED && echo '1' || echo '0'`
-fi
-if [ $NEEDS_FETCH = '1' ]; then
-  wget https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/$OPENAPI_GENERATOR_VERSION/openapi-generator-cli-$OPENAPI_GENERATOR_VERSION.jar -O openapi-generator-cli.jar -q
-fi
-
 # build
-java -jar openapi-generator-cli.jar generate \
+docker run --rm -v "${PWD}:/local" -u $(id -u) openapitools/openapi-generator-cli:v$OPENAPI_GENERATOR_VERSION generate \
   -i https://raw.githubusercontent.com/traPtitech/traQ/master/docs/v3-api.yaml \
   -g go \
-  -c config.yaml
+  -c /local/config.yaml \
+  -o /local
 mv README.md client.md
 mv README.md.bak README.md
 
